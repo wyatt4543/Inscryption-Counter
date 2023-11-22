70d0ce98cd60aafacb90350d8a6393484a0748eb
wyatt4543 <9yearsoldwhenstarted@gmail.com>
2023-11-22 17:39:44 -0600

    edit html

--- counter.js
+++ counter.js
@@ -3,10 +3,8 @@ let counterL = 0;
 
 const counterValueR = document.getElementById('counter-valueR');
 const incrementBtnR = document.getElementById('increment-btnR');
-const decrementBtnR = document.getElementById('decrement-btnR');
 const counterValueL = document.getElementById('counter-valueL');
 const incrementBtnL = document.getElementById('increment-btnL');
-const decrementBtnL = document.getElementById('decrement-btnL');
 
 // To increment the value of counter
 incrementBtnR.addEventListener('click', () => {
@@ -16,26 +14,11 @@ incrementBtnR.addEventListener('click', () => {
 	counterValueL.innerHTML = counterL;
 });
 
-// To decrement the value of counter
-decrementBtnR.addEventListener('click', () => {
-	counterR--;
-	counterValueR.innerHTML = counterR;
-	counterL++;
-	counterValueL.innerHTML = counterL;
-});
 
 // To increment the value of counter
 incrementBtnL.addEventListener('click', () => {
 	counterL++;
 	counterValueL.innerHTML = counterL;
 	counterR--;
-	counterValueR.innerHTML = counterR;
-});
-
-// To decrement the value of counter
-decrementBtnL.addEventListener('click', () => {
-	counterL--;
-	counterValueL.innerHTML = counterL;
-	counterR++;
 	counterValueR.innerHTML = counterR;
 });
