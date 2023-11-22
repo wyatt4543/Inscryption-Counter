d71b40b4a7b4bd8775a32ed3ed671edab65c8bae
wyatt4543 <9yearsoldwhenstarted@gmail.com>
2023-11-22 17:34:29 -0600

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,8 +1,12 @@
 let counterR = 0;
+let counterL = 0;
 
 const counterValueR = document.getElementById('counter-valueR');
 const incrementBtnR = document.getElementById('increment-btnR');
 const decrementBtnR = document.getElementById('decrement-btnR');
+const counterValueR = document.getElementById('counter-valueL');
+const incrementBtnL = document.getElementById('increment-btnR');
+const decrementBtnL = document.getElementById('decrement-btnR');
 
 // To increment the value of counter
 incrementBtnR.addEventListener('click', () => {
@@ -14,4 +18,16 @@ incrementBtnR.addEventListener('click', () => {
 decrementBtnR.addEventListener('click', () => {
 	counterR--;
 	counterValueR.innerHTML = counterR;
+});
+
+// To increment the value of counter
+incrementBtnL.addEventListener('click', () => {
+	counterL++;
+	counterValueL.innerHTML = counterL;
+});
+
+// To decrement the value of counter
+decrementBtnL.addEventListener('click', () => {
+	counterL--;
+	counterValueL.innerHTML = counterL;
 });
