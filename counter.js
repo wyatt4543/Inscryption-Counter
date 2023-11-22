2e70c5268549cbbdbbc4472c2549bedba5430f76
wyatt4543 <9yearsoldwhenstarted@gmail.com>
2023-11-22 17:36:53 -0600

    Update counter.js

--- counter.js
+++ counter.js
@@ -5,29 +5,37 @@ const counterValueR = document.getElementById('counter-valueR');
 const incrementBtnR = document.getElementById('increment-btnR');
 const decrementBtnR = document.getElementById('decrement-btnR');
 const counterValueL = document.getElementById('counter-valueL');
-const incrementBtnL = document.getElementById('increment-btnR');
-const decrementBtnL = document.getElementById('decrement-btnR');
+const incrementBtnL = document.getElementById('increment-btnL');
+const decrementBtnL = document.getElementById('decrement-btnL');
 
 // To increment the value of counter
 incrementBtnR.addEventListener('click', () => {
 	counterR++;
 	counterValueR.innerHTML = counterR;
+	counterL--;
+	counterValueL.innerHTML = counterL;
 });
 
 // To decrement the value of counter
 decrementBtnR.addEventListener('click', () => {
 	counterR--;
 	counterValueR.innerHTML = counterR;
+	counterL++;
+	counterValueL.innerHTML = counterL;
 });
 
 // To increment the value of counter
 incrementBtnL.addEventListener('click', () => {
 	counterL++;
 	counterValueL.innerHTML = counterL;
+	counterR--;
+	counterValueR.innerHTML = counterR;
 });
 
 // To decrement the value of counter
 decrementBtnL.addEventListener('click', () => {
 	counterL--;
 	counterValueL.innerHTML = counterL;
+	counterR++;
+	counterValueR.innerHTML = counterR;
 });
