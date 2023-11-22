02a8b8160fd8d746a1a0940517b5a4f2d8947055
wyatt4543 <9yearsoldwhenstarted@gmail.com>
2023-11-22 17:13:55 -0600

    edit html

--- counter.js
+++ counter.js
@@ -1,26 +1,17 @@
-let counter = 0;
+let counterR = 0;
 
-const counterValue = document.getElementById('counter-value');
-const incrementBtn = document.getElementById('increment-btn');
-const decrementBtn = document.getElementById('decrement-btn');
-const resetBtn = document.querySelector('#reset');
+const counterValueR = document.getElementById('counter-valueR');
+const incrementBtnR = document.getElementById('increment-btnR');
+const decrementBtnR = document.getElementById('decrement-btnR');
 
 // To increment the value of counter
-incrementBtn.addEventListener('click', () => {
-	counter++;
-	counterValue.innerHTML = counter;
+incrementBtnR.addEventListener('click', () => {
+	counteRr++;
+	counterValueR.innerHTML = counter;
 });
 
 // To decrement the value of counter
-decrementBtn.addEventListener('click', () => {
-	counter--;
-	counterValue.innerHTML = counter;
+decrementBtnR.addEventListener('click', () => {
+	counterR--;
+	counterValueR.innerHTML = counter;
 });
-
-// To reset the counter to zero
-resetBtn.addEventListener('click', reset);
-
-function reset() {
-	counter = 0;
-	counterValue.innerHTML = counter;
-}
