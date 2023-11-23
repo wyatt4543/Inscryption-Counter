8c0aeca37bcab031d2d75be69a48802013c84960
wyatt4543 <9yearsoldwhenstarted@gmail.com>
2023-11-22 19:06:38 -0600

    edit html

--- counter.js
+++ counter.js
@@ -1,10 +1,10 @@
 let counterR = 0;
 let counterL = 0;
 
-const counterValueR = document.getElementById("counter-valueR");
-const incrementBtnR = document.getElementById("increment-btnR");
-const counterValueL = document.getElementById("counter-valueL");
-const incrementBtnL = document.getElementById("increment-btnL");
+const counterValueR = document.querySelector("counter-valueR");
+const incrementBtnR = document.querySelector("increment-btnR");
+const counterValueL = document.querySelector("counter-valueL");
+const incrementBtnL = document.querySelector("increment-btnL");
 
 // To increment the value of counter
 incrementBtnR.addEventListener('click', () => {
